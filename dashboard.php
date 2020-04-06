8a70053f1cbed15f3ab85b0f71078b41e19b7cf3
Dave Robinson <robboz4@gmail.com>
2020-04-05 17:01:55 -0700

    Updated to Version 2
    Web page only reflects update data.

--- dashboard.php
+++ dashboard.php
@@ -11,11 +11,11 @@ table, th, td {
 
 </head>
 <body>
-<h1 align=\"center\"> Mock up dashboard  for monitoring motors under test. (A.0.1)</h1>
+<h1 align=\"center\"> Mock up dashboard  for monitoring motors under test. (A.2.0)</h1>
 
 Press Button  to start simluated updating of csv file. It will run a 10x loop to add new data. <br>
-The data is serial number, revs and temperature. Numbers are simply generated via python random code generator 1-10.
-Hit refresh after pressing the run button to see the data. The page  will refresh every 10 seconds after that.<br>
+The data is serial number, revs and temperature. Numbers are simply generated via python random code generator 1-10.<br>
+Wait about 10 seconds after pressing the run button, refresh the page to see the data. The page  will refresh every 10 seconds after that.<br>
 
 <form action="dashboard.php" method="POST">
 <button name="submit"  value="start" class="green">Start demo run.</button>
@@ -28,6 +28,37 @@ Hit refresh after pressing the run button to see the data. The page  will refres
 Dave Robinson 4/4/2020
 
 */
+
+function   check_entry($motor, $dashboard)
+
+{
+	$data = FALSE;
+	$arrlength = count($dashboard);
+	for($x = 0; $x < $arrlength; $x++){
+                foreach($dashboard[$x] as $value){
+//
+			if ($value == $motor[0]) {
+				$dashboard[$x] = $motor;
+				$data =  TRUE;
+			}
+                }
+
+	 }
+	if ( $data == FALSE) {
+		
+		
+		$new_array = array_push($dashboard, $motor);
+
+	}
+
+
+
+	return($dashboard);
+
+
+} 
+
+
 if(isset($_POST['submit'])){
 
 
@@ -38,20 +69,46 @@ if(isset($_POST['submit'])){
 
 else {
          $stream =  fopen("motor.csv",  "r");
+//	 echo " Dashboard is: ";
+/*	 $dashboard = array(
+				array( 'serial'=>"", 'rev'=>"", 'temp'=>"")
+			   );
+
+*/
+	$dashboard = array();
+//	 print_r($dashboard);
+//	 echo "      <br>";
          echo  "<table>";
          echo "<caption>Motor data </caption>";
-         echo "<tr> <th> Motor S/N </th><th> Revs </th><th>  Temperature </th></tr>";
+         echo "<tr> <th> Motor S/N </th><th> Revs </th><th>  Temperature </th> <th> Timestamp </th></tr>";
+
          while (($motor = fgetcsv($stream)) !== false) {
+	
+	        $dashboard = check_entry($motor, $dashboard);
+//		echo "entries " . count($dashboard) . "<br>";
+//		var_dump($dashboard);
+//		echo '<pre>' . var_export($dashboard, true) . '</pre>';
+		}
+//	  echo "Main loop <br>";
 
-	  echo "<tr><td> " . $motor[0] . "</td>" ;
-	  echo "<td>" . $motor[1] . "</td>" ;
-	  echo "<td>" . $motor [2] . "</td></tr>";
-
-
-         }
-         echo "</table>";
 	fclose($stream);
-
+/*	echo count($dashboard) . "= Number in array<br>";
+	foreach ($dashboard as $c) {
+		while (list($k, $v) = each($c)){
+			echo  $v . "<br>"; 
+		}
+		"<br>";
+	}
+*/
+	$arrlength = count($dashboard);
+	for($x = 0; $x < $arrlength; $x++) {
+        echo "<tr>";   
+	foreach($dashboard[$x] as $value){
+		echo "<td>" . $value . "</td>";
+		}
+           echo "</tr>";
+        }
+	echo "</table>";
 }
 
 ?>
--- motor_stream.py
+++ motor_stream.py
@@ -3,6 +3,9 @@
 # Dave Robinson 4/4/20
 
 import time
+import datetime
+
+
 # generate random integer values
 from random import seed
 from random import randint
@@ -17,10 +20,11 @@ while  i < 10:
         	value = randint(0, 10)
 #        	print(value)
 	if value != 0:	
+		x = datetime.datetime.now()
 		rev = value * 1000
-		temp = value * 32
-		msn = value + value
-		entry = "PBM00-" + str(msn) + "," + str(rev) + "," + str(temp) + "\n"
+		temp = value * 32 + x.minute
+		msn = value 
+		entry = "PBM00-" + str(msn) + "," + str(rev) + "," + str(temp) + "," + str(x) + "\n"
 
 		file_object = open('motor.csv', 'a')
 		file_object.write(entry)
