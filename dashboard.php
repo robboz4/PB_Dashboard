1a3d6390f7fde61947ee3f7e38e0e8dba61acf2a
Dave Robinson <robboz4@gmail.com>
2020-04-08 16:32:16 -0700

    Added limtes for RPM and TEMP
    Added a simple limit check that turns the text red for RPM and TEMP if they execeed 7000rpm and 300 Degrees.
    
    Modified feed to add a bit more randomness and run for 20 loops at 1 second intervals.

--- dashboard.php
+++ dashboard.php
@@ -5,17 +5,24 @@ table, th, td {
   border: 1px solid black;
   border-collapse: collapse;
 }
+td.hot{
+  color:red;
 </style>
 
 <meta http-equiv="refresh" content="10">
 
 </head>
 <body>
-<h1 align=\"center\"> Mock up dashboard  for monitoring motors under test. (A.2.1)</h1>
 
-Press Button  to start simluated updating of table from a csv file/stream. It will run a 10x loop to add new or updated  data. <br>
+
+<h1 align=\"center\"> Mock up dashboard  for monitoring motors under test. (A.2.2)</h1>
+
+Press Button  to start simluated updating of table from a csv file/stream. It will run a 20x loop to add new or updated  data. <br>
 The data is serial number, revs and temperature. Numbers are simply generated via python random code generator 1-10.<br>
 The page  will refresh every 10 seconds after that.<br>
+RPM  entries will turn red if they exceed 7000 rpm<br>
+TEMP entries will turn red if they exceed 300 degrees
+
 
 <form action="dashboard.php" method="POST">
 <button name="submit"  value="start" class="green">Start demo run.</button>
@@ -30,12 +37,17 @@ Dave Robinson 4/4/2020
 A.0.0 Simple reading a csv file an dupdating the screen
 A.2.0 Updates table with new or updated data of eeiting entries. (4/5/2020)
 A.2.1 Removed the need to refresh page to get data after pressing the start button (4/6/2020)
+A.2.2 Added color chnages to RPM and temperature to demo overlimits seetings (4/8/2020)
 
 
 
 */
 
 function   check_entry($motor, $dashboard)
+/*
+function to check if serial number is already reporting.
+
+*/
 
 {
 	$data = FALSE;
@@ -63,7 +75,34 @@ function   check_entry($motor, $dashboard)
 
 
 } 
+function check_range($cell_offset, $cell_value)
 
+
+/*Function to see if rpm or temp go over set limits. Turn  text red to indicate vaule is over limit
+
+*/
+{
+        if ($cell_offset == 2 ) {
+                if ( $cell_value > 300) {
+                        $result =  "<td class=\"hot\"   >  <b>  " . $cell_value . "<b/> </td>";
+                }
+                else {
+                        $result = "<td>" . $cell_value . "</td>";
+                }
+        }
+        if ($cell_offset == 1 ){
+                if ( $cell_value >= 7000) {
+                        $result =  "<td class=\"hot\" >  <b>  " . $cell_value . "<b/> </td>";
+                }
+                else {
+                        $result = "<td>" . $cell_value . "</td>";
+                }
+        }
+        
+        
+        
+        return($result);
+}
 
 if(isset($_POST['submit'])){
 
@@ -76,25 +115,45 @@ if(isset($_POST['submit'])){
 
 else {
          $stream =  fopen("motor.csv",  "r");
-	$dashboard = array();
+         $dashboard = array();
          echo  "<table>";
          echo "<caption>Motor data </caption>";
          echo "<tr> <th> Motor S/N </th><th> Revs </th><th>  Temperature </th> <th> Timestamp </th></tr>";
 
          while (($motor = fgetcsv($stream)) !== false) {
-	
-	        $dashboard = check_entry($motor, $dashboard);
-	$arrlength = count($dashboard);
-	for($x = 0; $x < $arrlength; $x++) {
-        echo "<tr>";   
-	foreach($dashboard[$x] as $value){
-		echo "<td>" . $value . "</td>";
-		}
-           echo "</tr>";
-        }
-	echo "</table>";
-}
+        
+                $dashboard = check_entry($motor, $dashboard);
+                }
+                fclose($stream);
+                $arrlength = count($dashboard);
+                for($x = 0; $x < $arrlength; $x++) {
+                        $y=0;
+                        echo "<tr>";   
+                        foreach($dashboard[$x] as $value){
+                                if ( $y == 2) {
+//                                      echo "<td> <b> " . $value . "</b></td>";
+                                        $new_value=check_range($y, $value);
+                                        echo $new_value;
+                                        $y +=1;
+                                }
 
+                                elseif ( $y == 1) {
+//                                      echo "<td> <b> " . $value . "</b></td>";
+                                        $new_value=check_range($y, $value);
+                                        echo $new_value;
+                                        $y +=1;
+                                }
+                                else {
+                                        echo "<td>" . $value . "</td>";
+                                        $y +=1;
+                                }
+                                
+                        }
+                        echo "</tr>";
+                }
+        
+        echo "</table>";
+}
 ?>
 
 
--- motor_stream.py
+++ motor_stream.py
@@ -1,7 +1,9 @@
 #Data Generator  for motor dashboard
 
 # Dave Robinson 4/4/20
-
+# Tweaked random number generator. Still crude
+# but enough to make  the limits trigger on the dashboard.
+# Also added a loop of 20 and reduced timing to 1 second.
 import time
 import datetime
 
@@ -14,22 +16,28 @@ seed(1)
 # generate some integers
 
 i = 0
-while  i < 10:
+while  i < 20:
 	
 	for _ in range(10):
         	value = randint(0, 10)
 #        	print(value)
 	if value != 0:	
 		x = datetime.datetime.now()
-		rev = value * 1000
-		temp = value * 32 + x.minute
+		for _ in range(10):
+                	rev_value = randint(0, 10)
+		rev = 10000 - (rev_value * 30 * x.second)
+		if  rev < 0 :	
+			rev= rev * -1 
+		for _ in range(10):
+                	temp_value = randint(0, 10)
+			temp = temp_value * 32 + x.minute
 		msn = value 
 		entry = "PBM00-" + str(msn) + "," + str(rev) + "," + str(temp) + "," + str(x) + "\n"
-
+#		print(entry)
 		file_object = open('motor.csv', 'a')
 		file_object.write(entry)
 		file_object.close()
-	time.sleep(10)
+	time.sleep(1)
 	i += 1
 
 
